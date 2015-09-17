e6b3ba4a8740ee4e76c6b1f6a0d17a6a447362e2
Alfonso Aparicio <aapariciog91@gmail.com>
2015-09-17 11:15:03 +0200

    Update Ultrasonic.h
    Added internal variables.
    
    Added constructor function declaration.

--- Ultrasonic.h
+++ Ultrasonic.h
@@ -1,1 +1,26 @@
+/*
+Ultrasonic.h - Library for using with HC-SR04 sensor
+Created by Alfonso Aparicio González, September 17, 2015
+Realesed into the public domain
+*/
 
+#ifndef Ultrasonic_h
+#define Ultrasonic_h
+
+#include "Arduino.h"
+
+class Ultrasonic
+{
+  public:
+  Ultrasonic(byte vcc, byte echo, byte trig, int limit); //Constructor
+  
+  private:
+  _vcc,
+  _echo;
+  _trig;
+  _limit
+}
+
+
+
+#endif
