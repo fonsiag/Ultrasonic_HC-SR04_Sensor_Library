cda5153c838878db4e7f6cffe2f40cbfb3563231
Alfonso Aparicio <aapariciog91@gmail.com>
2015-09-18 13:40:49 +0200

    Update Ultrasonic.h
    Adding the las functions definitions (distance and reset).
    
    Adding coments to explain the written code

--- Ultrasonic.h
+++ Ultrasonic.h
@@ -13,12 +13,13 @@ class Ultrasonic
 {
   public:
   Ultrasonic(byte vcc, byte echo, byte trig, int limit); //Constructor
-  
+  float distance() //Calculates de distance to an object
+  void reset() //Reset the Ultrasonic Sensor when bad measures are done.
   private:
-  _vcc,
-  _echo;
-  _trig;
-  _limit
+  _vcc, //Control pin for voltage supply of the sensor, used to reset it.
+  _echo; //Input pin for reciving the echo response
+  _trig; //Output pin for sending the triggering signal
+  _limit //Value of distance limit to measure in 'cm'
 }
 
 
